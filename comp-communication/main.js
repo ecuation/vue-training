b667f7f5080f5717bfabe85475141e36d10de2f5
Daniel Carvajal <d.carvajal.web@gmail.com>
2017-03-26 19:58:27 +0200

    vue component communication exercise

--- comp-communication/main.js
+++ comp-communication/main.js
@@ -1,10 +1,39 @@
+Vue.component('coupon', 
+{
+	template:`
+		<div>
+			<input type="text" @blur="applyVoucher" />
+
+			<slot v-if="couponHasBeenApplied"></slot>
+		</div>
+	`,
+	data(){
+		return {
+			couponHasBeenApplied: false
+		};
+	},
+	methods:
+	{
+		applyVoucher()
+		{
+			this.$emit('applied');
+			this.couponHasBeenApplied = true;
+		}
+	}
+
+});
+
+
 var app = new Vue(
 {
 	el: '#root',
 	data: {
-		
+		couponApplied: false
 	},
 	methods: {
+		onCouponApplied(){
+			this.couponApplied = true;
+		}
 
 	},
 	mounted() {
